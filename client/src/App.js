10e97369a10a098afa2f775044a167ab5ca9e741
Malkeet <malkeet1028@chitkara.edu.in>
2023-04-05 13:04:05 +0530

    path modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import ThemeContextProvider from './ContextApi/ThemeContextProvider';
+import ThemeContextProvider from './components/ContextApi/ThemeContextProvider';
 import ClassA from './components/class-components/ClassA';
-import List1 from './ContextChildrens/List1';
-import List2 from './ContextChildrens/List2';
-import ThemeToggle from './ContextChildrens/ThemeToggle';
-import AuthContextProvider from './ContextApi/AuthContextProvider';
-import List3 from './ContextChildrens/List3';
+import List1 from './components/ContextChildrens/List1';
+import List2 from './components/ContextChildrens/List2';
+import ThemeToggle from './components/ContextChildrens/ThemeToggle';
+import AuthContextProvider from './components/ContextApi/AuthContextProvider';
+import List3 from './components/ContextChildrens/List3';
 
 function App() {
   return (
